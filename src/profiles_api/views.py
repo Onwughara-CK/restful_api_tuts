aa084bada2ca0c401ee8479ad3e285f1ceea676c
Collins <kelechicollins.93@gmail.com>
2020-07-30 18:13:57 +0100

    add our first APIView

--- src/profiles_api/views.py
+++ src/profiles_api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class HelloApiView(APIView):
+    """
+    test api view
+    """
+
+    def get(self, request, pattern=None):
+        """
+        returns list of APIViews features
+        """
+
+        api_features = [
+            'gives you the most control over your logic',
+            'uses http methods as functions',
+            'is mapped manually to URLs'
+        ]
+
+        return Response({'message': 'Your list is ready', 'api_features': api_features})
